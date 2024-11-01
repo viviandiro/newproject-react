c7835a81415e806ee1c8ef56e3c06ccb12978252
VivianDiro <71907500+viviandiro@users.noreply.github.com>
2024-11-01 18:31:22 -0500

    Agrgar mas funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
 import "./index.css";
-import { Hourglass } from "react-loader-spinner";
+
+
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="Weather1">
       <h1>Weather App</h1>
-      <p>
+     
         <Weather />
-      </p>
-      <Hourglass
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="hourglass-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        colors={["#61dafb", "#72a1ed"]}
-      />
+      
+     
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { ColorRing } from "react-loader-spinner";
 
 export default function Weather() {
   let [city, setCity] = useState("");
   let [mostrar, setMostrar] = useState(false);
   let [weather, setWeather] = useState({});
+
+  const [isImageVisible, setIsImageVisible] = useState(true);
 
   function datos(response) {
     setMostrar(true);
@@ -16,8 +19,18 @@ export default function Weather() {
       description: response.data.weather[0].description,
     });
   }
+
   function searchCity(event) {
     event.preventDefault();
+    setIsImageVisible(false);
+
+    if (city === "") {
+      alert("El campo no puede estar vacío");
+      return;
+       
+     
+    }
+
     let apiKey = "1dbf926d3b4417bf379db7043bec1047";
     let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(urlApi).then(datos);
@@ -31,32 +44,79 @@ export default function Weather() {
       <input
         type="text"
         className="form"
-         placeholder="Enter city 🔍"
+        placeholder="Enter city 🔍"
         onChange={updateCity}
       />
-      <button type="submit" className="btn1">Search</button>
+      <button type="submit" className="btn1">
+        Search
+      </button>
     </form>
   );
 
-  if (mostrar) {
-    return (
-      <div className="Weather">
-        {form}
-        <h2> {city} </h2>
-        <ul className="list1">
-          <li className="Temp">{Math.round(weather.temperature)}°C</li>
-          <li className="description" id="description">
-            Description: {weather.description}
-          </li>
-          <li className="hum">Humidity: {weather.humidity} % </li>
-          <li className="wind">Wind: {weather.wind} km/h</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
-      </div>
-    );
-  } else {
-    return form;
-  }
+  const renderContent = () => {
+    if (mostrar) {
+      return (
+        <div className="Weather">
+          {form}
+          <h2> {city} </h2>
+          <ul className="list1">
+            <li className="Temp">{Math.round(weather.temperature)}°C</li>
+            <li className="description" id="description">
+              Description: {weather.description}
+            </li>
+            <li className="hum">Humidity: {weather.humidity} % </li>
+            <li className="wind">Wind: {weather.wind} km/h</li>
+            <li>
+              <img src={weather.icon} alt={weather.description} />
+            </li>
+          </ul>
+          <div className="row justify-content-center flex">
+            <div className="col">
+              <h3>MON </h3>
+              <h1>🌤️ </h1>
+            </div>
+            <div className="col">
+              <h3>TUE </h3>
+              <h1>☀️ </h1>
+            </div>
+
+            <div className="col">
+              <h3>WED </h3>
+              <h1>🌦️ </h1>
+            </div>
+
+            <div className="col">
+              <h3>THU </h3>
+              <h1>⛈️ </h1>
+            </div>
+            <div className="col">
+              <h3>FRI</h3>
+              <h1>☁️</h1>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return form;
+    }
+  };
+
+  return (
+    <div className="App">
+      {renderContent()}
+      {isImageVisible && (
+        <div>
+          <ColorRing
+            visible={true}
+            height="150"
+            width="120"
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={["#011627", "#15A0BF", "#24b79c", "#abbd81", "#849b87"]}
+          ></ColorRing>
+        </div>
+      )}
+    </div>
+  );
 }
